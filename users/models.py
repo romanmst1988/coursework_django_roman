5ad5e5646edd0ec0465684e0579d840ec6731636
Роман Мстиславский <mstislavskijr@gmail.com>
2025-11-23 22:01:19 +0300

    fix code23_11_2025

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=150)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     country = models.CharField(max_length=50, blank=True, null=True)
