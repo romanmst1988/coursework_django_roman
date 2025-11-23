11370d4558e9b89ea6ac6ea8ce3deda54549de65
Роман Мстиславский <mstislavskijr@gmail.com>
2025-11-23 21:13:26 +0300

    fix-name-sait

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('index')
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
